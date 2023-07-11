b538830eb9211bd52416f57ba61375622d600510
Samhita <meetu2005@icloud.com>
2023-07-11 17:34:39 -0400

    Initial Commit

---
+++ Estee Launder App/Estee_Launder_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estee_Launder_AppApp.swift
+//  Estee Launder App
+//
+//  Created by Samhita Prabhu on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Estee_Launder_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
